1d44ff947aa5c62b7f2be60ec855e839268854c2
unknown <st13vv@gmail.com>
2025-10-18 20:44:24 +0300

    added cart item dropdown

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -7,36 +7,34 @@ export function CartProvider({ children }) {
 
   const addToCart = (product) => {
     setCart((prev) => {
-      const existing = prev.find((item) => {
-        item.id === product.id;
-      });
+      const existing = prev.find((item) => item.id === product.id);
       if (existing) {
-        return prev.map((item) => {
-          item.id === product ? { ...item, qty: item.qty + 1 } : item;
-        });
+        return prev.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item
+        );
       }
       return [...prev, { ...product, qty: 1 }];
     });
   };
 
-  //   useEffect(() => {
-  //     const fetchCart = async () => {
-  //       try {
-  //         const res = await fetch('api/cart');
-  //         if (!res.ok) {
-  //           throw new Error('Faild to Fetch cart');
-  //         }
-  //         const data = await res.json();
-  //         setCart(data);
-  //       } catch (err) {
-  //         setError(err.message);
-  //       } finally {
-  //         setLoading(false);
+  // useEffect(() => {
+  //   const fetchCart = async () => {
+  //     try {
+  //       const res = await fetch('api/cart');
+  //       if (!res.ok) {
+  //         throw new Error('Faild to Fetch cart');
   //       }
-  //     };
+  //       const data = await res.json();
+  //       setCart(data);
+  //     } catch (err) {
+  //       setError(err.message);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-  //     fetchCart();
-  //   }, []);
+  //   fetchCart();
+  // }, []);
 
   return (
     <CartContext.Provider value={{ cart, addToCart }}>
